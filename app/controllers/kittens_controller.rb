c7b4d0d6622c5061ef1fdb8c71ab165ffc81bb67
Michael-Mehta <supermichael2001@gmail.com>
2022-12-01 11:17:14 -0700

    filled out 6 controller actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,24 +1,54 @@
 class KittensController < ApplicationController
     
     def index
+        @kittens = Kitten.all
     end
 
     def show
+        @kitten = Kitten.find(params[:id])
     end
 
     def new
+        @kitten = Kitten.new
     end
 
     def create
-    end
+        @kitten = Kitten.new(kitten_params)
+        
+        if @kitten.save
+            flash[:success] = "Kitten Created!"
+            redirect_to @kitten
+        else
+            flash.now[:error] = "Kitten details are invalid"
+            render :new, status: :unprocessable_entity
+        end
+    
+    end 
+
 
     def edit
+        @kitten = Kitten.find(params[:id])
     end
 
     def update
+        @kitten = Kitten.find(params[:id])
+
+        if @kitten.update(kitten_params)
+            redirect_to @kitten
+        else
+            flash.now[:error] = "Kitten details are invalid"
+            render :new, status: :unprocessable_entity
+        end
     end
 
     def delete
     end
-    
+
+
+    private
+
+    def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+
 end
